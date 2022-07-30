6f263640f3701d188cc300f0c17648023c7fbef4
yuvarajmkuruba <yuvarajmkuruba@gmail.com>
2022-07-30 12:33:56 +0530

    Changed

---
+++ Arrays/FindtheDuplicateNumber.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public enum FindtheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        for(int i = 0; i<nums.length; i++){
+              if(nums[Math.abs(nums[i])] < 0){
+                  return Math.abs(nums[i]);
+              }else {
+                  nums[Math.abs(nums[i])] = nums[Math.abs(nums[i])] * -1;
+              }
+          }
+          return -1;
+      }
+}
