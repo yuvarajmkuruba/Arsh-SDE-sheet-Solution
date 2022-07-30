2647cfcb0bd4e3a28779e7892158f5fedacbe0da
yuvarajmkuruba <yuvarajmkuruba@gmail.com>
2022-07-30 19:45:58 +0530

    hey

---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int k=nums.length-1;
+        int i=0;
+        while(i<=k){
+            if(nums[i]==0){
+                
+                nums[k]=0;
+            }
+        }
+    }
+}
